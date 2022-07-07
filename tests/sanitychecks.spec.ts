bbc088687e444aaded61773849de657f0cfcdd25
Jon Bell <github@lot23.com>
2022-07-07 18:50:41 +1200

    tesrting broken test

--- tests/sanitychecks.spec.ts
+++ tests/sanitychecks.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Does the page load', async ({ page }) => {
   await page.goto('http://localhost:3000');
   const title = page.locator('TITLE');
-  await expect(title).toHaveText('Crate Digger');
+  await expect(title).toHaveText('Crate Diggah');
 });
