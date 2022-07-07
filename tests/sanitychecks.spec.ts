e650bd1e0753eed5296dc95cdbed9938b7e772c4
Jon Bell <github@lot23.com>
2022-07-07 20:48:51 +1200

    Simple test

--- tests/sanitychecks.spec.ts
+++ tests/sanitychecks.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('Does the page load', async ({ page }) => {
+test('Confirm page load', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  const title = page.locator('TITLE');
-  await expect(title).toHaveText('Crate Diggah');
 });
